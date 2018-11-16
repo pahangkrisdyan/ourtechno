55210f959bdf1a5e2f36a214b95ce05339570703
pahangkrisdyan <etika.pahang.k.1998@gmail.com>
2018-11-16 16:50:35 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,300 @@
+<!-- Slide1 -->
+<section class="slide1">
+	<div class="wrap-slick1">
+		<div class="slick1">
+			<div class="item-slick1 item1-slick1" style="background-image: url(<?php echo base_url();?>aset/images/value-of-its.jpg);">
+				<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
+					<strong><h1 class="caption1-slide1 xl-text2 t-centerp-b-6 animated visible-false m-b-22" data-appear="fadeInUp">
+						Produk Unik Mahasiswa ITS
+					</h1></strong>
+
+					<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
+						<!-- Button -->
+						<a href="<?php echo base_url();?>shop" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
+								BELANJA!
+							</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="item-slick1 item2-slick1" style="background-image: url(<?php echo base_url();?>aset/images/rotan.jpg);">
+				<div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
+					<strong><h1 class="caption1-slide1 xl-text2 t-center p-b-6 animated visible-false" data-appear="fadeInUp">
+						Berbagai Macam Inovasi Produk
+					</h1></strong>
+
+					<span class="caption2-slide1 m-text1 t-center animated visible-false m-b-33" data-appear="lightSpeedIn">
+							Asli ITS Surabaya
+						</span>
+
+					<div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="slideInUp">
+						<!-- Button -->
+						<a href="<?php echo base_url();?>shop" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
+								BELANJA!
+							</a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+<!-- Banner -->
+<div class="banner bgwhite p-t-40 p-b-40">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
+				<!-- block1 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="<?php echo base_url();?>aset/images/pakaian.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="<?php echo base_url();?>shop" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+								Fashion
+							</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
+				<!-- block1 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="<?php echo base_url();?>aset/images/craft.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="<?php echo base_url();?>shop" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4" style="font-size: 14px;">
+								Kerajinan
+							</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-10 col-md-8 col-lg-4 m-l-r-auto">
+				<!-- block1 -->
+				<div class="block1 hov-img-zoom pos-relative m-b-30">
+					<img src="<?php echo base_url();?>aset/images/jasa.jpg" alt="IMG-BENNER">
+
+					<div class="block1-wrapbtn w-size2">
+						<!-- Button -->
+						<a href="<?php echo base_url();?>shop" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
+								Jasa
+							</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!-- Our product -->
+<section class="bgwhite p-t-45 p-b-58">
+	<div class="container">
+		<div class="sec-title p-b-22">
+			<h3 class="m-text5 t-center">
+				Produk laris bulan ini
+			</h3>
+		</div>
+
+		<!-- Tab01 -->
+		<div class="tab01">
+			<!-- Tab panes -->
+			<div class="tab-content p-t-35">
+				<!-- - -->
+				<div class="tab-pane fade show active" id="best-seller" role="tabpanel">
+					<div class="row">
+						<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
+							<!-- Block2 -->
+							<div class="block2">
+								<div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+									<img src="<?php echo base_url();?>aset/images/pakaian.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+									<div class="block2-overlay trans-0-4">
+										<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+												<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+												<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+											</a>
+
+										<div class="block2-btn-addcart w-size1 trans-0-4">
+											<!-- Button -->
+											<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+													Pesan Sekarang
+												</button></a>
+										</div>
+									</div>
+								</div>
+
+								<div class="block2-txt p-t-20">
+									<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+											Baju All-in-One 
+										</a>
+
+									<span class="block2-price m-text6 p-r-5">
+											Rp 30.000
+										</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
+							<!-- Block2 -->
+							<div class="block2">
+								<div class="block2-img wrap-pic-w of-hidden pos-relative">
+									<img src="<?php echo base_url();?>aset/images/pakaian.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+									<div class="block2-overlay trans-0-4">
+										<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+												<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+												<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+											</a>
+
+										<div class="block2-btn-addcart w-size1 trans-0-4">
+											<!-- Button -->
+											<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+													Pesan Sekarang
+												</button></a>
+										</div>
+									</div>
+								</div>
+
+								<div class="block2-txt p-t-20">
+									<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+											Tas All-in-One
+										</a>
+
+									<span class="block2-price m-text6 p-r-5">
+											Rp 40.000
+										</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
+							<!-- Block2 -->
+							<div class="block2">
+								<div class="block2-img wrap-pic-w of-hidden pos-relative">
+									<img src="<?php echo base_url();?>aset/images/pakaian.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+									<div class="block2-overlay trans-0-4">
+										<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+												<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+												<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+											</a>
+
+										<div class="block2-btn-addcart w-size1 trans-0-4">
+											<!-- Button -->
+											<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+													Pesan Sekarang
+												</button></a>
+										</div>
+									</div>
+								</div>
+
+								<div class="block2-txt p-t-20">
+									<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+											Dompet All-in-One
+										</a>
+
+									<span class="block2-price m-text6 p-r-5">
+											Rp 20.000
+										</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
+							<!-- Block2 -->
+							<div class="block2">
+								<div class="block2-img wrap-pic-w of-hidden pos-relative">
+									<img src="<?php echo base_url();?>aset/images/pakaian.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+									<div class="block2-overlay trans-0-4">
+										<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+												<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+												<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+											</a>
+
+										<div class="block2-btn-addcart w-size1 trans-0-4">
+											<!-- Button -->
+											<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+													Pesan Sekarang
+												</button></a>
+										</div>
+									</div>
+								</div>
+
+								<div class="block2-txt p-t-20">
+									<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+											Sepatu All-in-One
+										</a>
+
+									<span class="block2-price m-text6 p-r-5">
+											Rp 30.000
+										</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<!-- Banner video -->
+<section class="parallax0 parallax100" style="background-image: url(<?php echo base_url();?>aset/images/banner.jpg);">
+	<div class="overlay0 p-t-190 p-b-200">
+		<div class="flex-col-c-m p-l-15 p-r-15">
+			<span class="m-text9 p-t-45 fs-20-sm">
+					Tertarik Menjadi
+				</span>
+
+			<h3 class="l-text1 fs-35-sm">
+				Mitra OurTechno?
+			</h3>
+
+			<span class="btn-play s-text4 hov5 cs-pointer p-t-25" data-toggle="modal" data-target="#modal-video-01">
+					Klik Disini
+				</span>
+		</div>
+	</div>
+</section>
+
+<!-- Shipping -->
+<section class="shipping bgwhite p-t-62 p-b-46">
+	<div class="flex-w p-l-15 p-r-15">
+		<div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 respon1">
+			<h4 class="m-text12 t-center">
+				Pesan Produk yang diinginkan
+			</h4>
+
+			<span class="s-text11 t-center">
+					Pilih &amp; pesan produk yang diinginkan
+				</span>
+		</div>
+
+		<div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 bo2 respon2">
+			<h4 class="m-text12 t-center">
+				Tentukan Tempat Pengiriman Produk
+			</h4>
+
+			<span class="s-text11 t-center">
+					Tentukan jasa &amp; tempat pengiriman produk
+				</span>
+		</div>
+
+		<div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 respon1">
+			<h4 class="m-text12 t-center">
+				Lakukan Pembayaran
+			</h4>
+
+			<span class="s-text11 t-center">
+					Lakukan pembayaran &amp; tunggu produk datang
+				</span>
+		</div>
+	</div>
+</section>
---
+++ application/views/shop.php
@@ -0,0 +1,292 @@
+<!-- Title Page -->
+<section class="bg-title-page p-t-50 p-b-40 flex-col-c-m" style="background-image: url(<?php echo base_url()?>aset/images/shop.jpg);">
+	<h2 class="l-text2 t-center">
+		Belanja Produk
+	</h2>
+</section>
+
+
+<!-- Content page -->
+<section class="bgwhite p-t-55 p-b-65">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
+				<div class="leftbar p-r-20 p-r-0-sm">
+					<!--  -->
+					<div class="search-product pos-relative bo4 of-hidden">
+						<input class="s-text7 size6 p-l-23 p-r-50" type="text" id="search" name="search" placeholder="Search Products...">
+
+						<button class="flex-c-m size5 ab-r-m color2 color0-hov trans-0-4" onclick="searchProduk()">
+								<i class="fs-12 fa fa-search" aria-hidden="true"></i>
+							</button>
+					</div>
+					<div class="filter-color p-t-22 p-b-50 bo3">
+					</div>
+					<h4 class="m-text14 p-b-7">
+						Kategori
+					</h4>
+
+					<ul class="p-b-54">
+						<li class="p-t-4">
+							<a href="#" class="s-text13 active1">
+									All
+								</a>
+						</li>
+
+						<li class="p-t-4">
+							<a href="#" class="s-text13">
+									Fashion
+								</a>
+						</li>
+
+						<li class="p-t-4">
+							<a href="#" class="s-text13">
+									Kerajinan
+								</a>
+						</li>
+
+						<li class="p-t-4">
+							<a href="#" class="s-text13">
+									Jasa
+								</a>
+						</li>
+					</ul>
+
+
+				</div>
+			</div>
+
+			<div class="col-sm-6 col-md-8 col-lg-9 p-b-50">
+				<!--  -->
+				<div class="flex-sb-m flex-w p-b-35">
+					<div class="flex-w">
+					</div>
+
+					<span class="s-text8 p-t-5 p-b-5">
+							Showing 1–12 of 16 results
+						</span>
+				</div>
+
+				<!-- Product -->
+				<div class="row">
+					<div class="col-sm-12 col-md-6 col-lg-4 p-b-50">
+						<!-- Block2 -->
+						<div class="block2">
+							<div class="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+								<img src="<?php echo base_url()?>aset/images/craft.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+								<div class="block2-overlay trans-0-4">
+									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+											<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+											<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+										</a>
+
+									<div class="block2-btn-addcart w-size1 trans-0-4">
+										<!-- Button -->
+										<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+												Pesan Sekarang
+											</button></a>
+									</div>
+								</div>
+							</div>
+
+							<div class="block2-txt p-t-20">
+								<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+										Contoh
+									</a>
+
+								<span class="block2-price m-text6 p-r-5">
+										Rp 30.000
+									</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-6 col-lg-4 p-b-50">
+						<!-- Block2 -->
+						<div class="block2">
+							<div class="block2-img wrap-pic-w of-hidden pos-relative">
+								<img src="<?php echo base_url()?>aset/images/craft.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+								<div class="block2-overlay trans-0-4">
+									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+											<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+											<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+										</a>
+
+									<div class="block2-btn-addcart w-size1 trans-0-4">
+										<!-- Button -->
+										<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+												Pesan Sekarang
+											</button></a>
+									</div>
+								</div>
+							</div>
+
+							<div class="block2-txt p-t-20">
+								<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+										Contoh
+									</a>
+
+								<span class="block2-price m-text6 p-r-5">
+										Rp 40.000
+									</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-6 col-lg-4 p-b-50">
+						<!-- Block2 -->
+						<div class="block2">
+							<div class="block2-img wrap-pic-w of-hidden pos-relative">
+								<img src="<?php echo base_url()?>aset/images/craft.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+								<div class="block2-overlay trans-0-4">
+									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+											<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+											<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+										</a>
+
+									<div class="block2-btn-addcart w-size1 trans-0-4">
+										<!-- Button -->
+										<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+												Pesan Sekarang
+											</button></a>
+									</div>
+								</div>
+							</div>
+
+							<div class="block2-txt p-t-20">
+								<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+										Contoh
+									</a>
+
+								<span class="block2-price m-text6 p-r-5">
+										Rp 20.000
+									</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-6 col-lg-4 p-b-50">
+						<!-- Block2 -->
+						<div class="block2">
+							<div class="block2-img wrap-pic-w of-hidden pos-relative">
+								<img src="<?php echo base_url()?>aset/images/craft.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+								<div class="block2-overlay trans-0-4">
+									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+											<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+											<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+										</a>
+
+									<div class="block2-btn-addcart w-size1 trans-0-4">
+										<!-- Button -->
+										<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+												Pesan Sekarang
+											</button></a>
+									</div>
+								</div>
+							</div>
+
+							<div class="block2-txt p-t-20">
+								<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+										Contoh
+									</a>
+
+								<span class="block2-price m-text6 p-r-5">
+										Rp 30.000
+									</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-6 col-lg-4 p-b-50">
+						<!-- Block2 -->
+						<div class="block2">
+							<div class="block2-img wrap-pic-w of-hidden pos-relative">
+								<img src="<?php echo base_url()?>aset/images/craft.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+								<div class="block2-overlay trans-0-4">
+									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+											<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+											<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+										</a>
+
+									<div class="block2-btn-addcart w-size1 trans-0-4">
+										<!-- Button -->
+										<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+												Pesan Sekarang
+											</button></a>
+									</div>
+								</div>
+							</div>
+
+							<div class="block2-txt p-t-20">
+								<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+										Contoh
+									</a>
+
+								<span class="block2-price m-text6 p-r-5">
+										Rp 25.000
+									</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-12 col-md-6 col-lg-4 p-b-50">
+						<!-- Block2 -->
+						<div class="block2">
+							<div class="block2-img wrap-pic-w of-hidden pos-relative">
+								<img src="<?php echo base_url()?>aset/images/craft.jpg" alt="IMG-PRODUCT" class="img720x960">
+
+								<div class="block2-overlay trans-0-4">
+									<a href="#" class="block2-btn-addwishlist hov-pointer trans-0-4">
+											<i class="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+											<i class="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+										</a>
+
+									<div class="block2-btn-addcart w-size1 trans-0-4">
+										<!-- Button -->
+										<a href="<?php echo base_url();?>shop/produk"><button class="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+												Pesan Sekarang
+											</button></a>
+									</div>
+								</div>
+							</div>
+
+							<div class="block2-txt p-t-20">
+								<a href="<?php echo base_url();?>shop/produk" class="block2-name dis-block s-text3 p-b-5">
+										Contoh
+									</a>
+
+								<span class="block2-price m-text6 p-r-5">
+										Rp 150.000
+									</span>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<!-- Pagination -->
+				<div class="pagination flex-m flex-w p-t-26">
+					<a href="#" class="item-pagination flex-c-m trans-0-4 active-pagination">1</a>
+					<a href="#" class="item-pagination flex-c-m trans-0-4">2</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<script type="text/javascript">
+	function searchProduk() {
+		var inpt = document.getElementById('search');
+		if (inpt.value == "") {
+			alert("Masukkan kata kunci");
+		} else {
+			window.location.href = "<?php echo base_url();?>shop/search/" + inpt.value;
+		}
+	}
+
+</script>
